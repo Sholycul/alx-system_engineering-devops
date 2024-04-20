abf4efc381d7f696998f14ba9c998aec5952f839
Sholycul <michaeloluwasholaiji2020@gmail.com>
2024-04-20 11:53:19 -0700

    Connected tom my Ubuntu server

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,13 @@
 # set up SSH configurations
 
-# Ensure that password authentication is disabled
-file_line { 'Turn off passwd auth':
-  path   => '/etc/ssh/sshd_config',
-  line   => 'PasswordAuthentication no',
-  match  => '^#?PasswordAuthentication',
-}
-
-# Declare the identity file to be used by SSH client
-file_line { 'Declare identity file':
+file_line { 'Turn off the password authentification':
+  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school',
-  match  => '^#?IdentityFile',
+  line   => '    PasswordAuthentication no',
 }
 
+file_line { 'the identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
